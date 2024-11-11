efff8fe1f2c64d9142f9f07e7657443570b44912
Seo Jinmann <sjmmics@gmail.com>
2024-11-11 19:30:26 +0900

    feat : ($Constants) 상수 클래스 추가
    Constants: 여러 클래스에서 공통으로 쓰는 상수를 저장한다.

---
+++ src/main/java/store/util/Constants.java
@@ -0,0 +1,12 @@
+package store.util;
+
+/** 여러 클래스에서 쓰이는 상수를 저장한 클래스 */
+public final class Constants {
+    
+    private Constants() {}
+    
+    public static String EXCEPTION_MESSAGE_PREFIX = "[ERROR] ";
+
+    public static final int BONUS_COUNT = 1;
+
+}
