efa42fdf3879a0e75d3f031ee85661d37d89dbfa
sjmmics <sjmmics@gmail.com>
2024-11-11 23:01:41 +0900

    test : 테스트 코드 추가
    프로모션 재고 초과 주문시 프로모션 재고 소진 부분 갱신 기능 테스트

--- src/test/java/store/domain/OrderQuantityAdjusterTest.java
+++ src/test/java/store/domain/OrderQuantityAdjusterTest.java
@@ -3,13 +3,18 @@ package store.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import store.model.discount.Promotion;
+import store.model.inventory.Inventory;
 import store.model.inventory.Product;
+import store.model.inventory.Stock;
 import store.model.order.OrderDetails;
 import store.model.orderquantity.OrderProductQuantity;
 import store.model.orderquantity.OrderQuantityAdjuster;
 import store.model.orderquantity.OrderQuantityOption;
 import store.model.orderquantity.OrderQuantityOptions;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -24,12 +29,22 @@ public class OrderQuantityAdjusterTest {
     
     private static final Product PRODUCT = new Product("콜라", 1000, null);
     
-    private OrderProductQuantity quantity;
-    
+    private static final LocalDate START_DATE = LocalDate.of(2024, 1, 1);
+
+    private static final LocalDate END_DATE = LocalDate.of(2024, 12, 31);
+
     private static final int PROMOTION_STOCK_SHORTAGE = 3;
-    
+
+    private static final int PROMOTION_STOCK = 5;
+
+    private static final int REGULAR_STOCK = 10;
+
+    private static final int ORDER_QUANTITY = 8;
+
+    private OrderProductQuantity quantity;
+
     private OrderDetails orderDetails;
-    
+
     @BeforeEach
     void setQuantity() {
         this.quantity = OrderProductQuantity.init(ORIGINAL_TOTAL_QUANTITY);
@@ -90,5 +105,36 @@ public class OrderQuantityAdjusterTest {
         OrderQuantityAdjuster adjuster = new OrderQuantityAdjuster(orderDetails);
         adjuster.applyOfOrderDetailsAndQuantityOptions(options);
     }
-    
+
+    @Test
+    @DisplayName("프로모션 재고 초과 구매 주문 수량 일반 재고 차감 계산 테스트")
+    void exceedPromotionStockRegularDeductionQuantityTest() {
+        Inventory inventory = getInventory();
+        LocalDateTime orderTime = LocalDateTime.of(2024, 11, 11, 11, 11);
+        OrderDetails orderDetails = getOrderDetails();
+        OrderQuantityAdjuster adjuster = new OrderQuantityAdjuster(orderDetails);
+        adjuster.applyOfOrderDetailsAndInventory(inventory, orderTime);
+        for (Map.Entry<Product, OrderProductQuantity> entry : orderDetails.getEntrySet()) {
+            OrderProductQuantity quantity = entry.getValue();
+            assertEquals(quantity.getPromotion(), PROMOTION_STOCK);
+            assertEquals(quantity.getRegular(), ORDER_QUANTITY - PROMOTION_STOCK);
+        }
+    }
+
+    Inventory getInventory() {
+        Promotion promotion = new Promotion("2+1", START_DATE, END_DATE, 2);
+        Product product = new Product("탄산수", 1200, promotion);
+        Stock stock = new Stock();
+        stock.setPromotion(PROMOTION_STOCK);
+        stock.setRegular(REGULAR_STOCK);
+        return new Inventory(Map.of(product, stock));
+    }
+
+    private OrderDetails getOrderDetails() {
+        Promotion promotion = new Promotion("2+1", START_DATE, END_DATE, 2);
+        Product product = new Product("탄산수", 1200, promotion);
+        OrderProductQuantity quantity = OrderProductQuantity.init(ORDER_QUANTITY);
+        return new OrderDetails(Map.of(product, quantity));
+    }
+
 }
